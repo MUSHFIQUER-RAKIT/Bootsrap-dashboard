bdaf2ced5ad85955ed6162a74ae245922cbe43ee
MUSHFIQUER-RAKIT <mushfiqurrakit@gmail.com>
2023-11-19 21:47:52 +0600

    Fixed Bugs And Animations
    Added footer

--- script/script.js
+++ script/script.js
@@ -6,23 +6,22 @@ const search = document.querySelector('.search');
 const closeBtn = document.querySelector('.close-btn');
 
 searchBtn.addEventListener('click', function () {
-  console.log(search.classList.contains('active'));
-  if (search.classList.contains('active')) {
-    searchBox.value = '';
-  } else {
-    search.classList.add('active');
-    searchBox.focus();
-  }
+	if (search.classList.contains('active')) {
+		searchBox.value = '';
+	} else {
+		search.classList.add('active');
+		searchBox.focus();
+	}
 });
 
 closeBtn.addEventListener('click', function () {
-  search.classList.remove('active');
-  search.value = '';
+	search.classList.remove('active');
+	search.value = '';
 });
 // navigation search bar java script END
 
 // navigation DASHBOARD java script start
 const dash_btn = document.querySelector('.dash_btn');
 dash_btn.addEventListener('click', () => {
-  document.querySelector('.big').classList.toggle('expaned');
+	document.querySelector('.big').classList.toggle('expaned');
 });
